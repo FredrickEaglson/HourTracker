fa47cf8b64e42522f84199005c661b55a3a9c221
Caleb Baxter <baxtercaleba@gmail.com>
2025-11-27 21:59:21 -0600

    Deleted files

---
+++ auth/db.php
@@ -0,0 +1,7 @@
+<?php 
+$pass="eYY)]ajDe274n_qJ";
+$user = "hourtracker";
+$db = "hourtracker";
+$server = "localhost";
+
+find
--- index.php
+++ index.php
@@ -1,8 +1,1 @@
-<?php 
-session_start();
-if (isset($_SESSION['loggedin'])&&$_SESSION['loggedin']==true)
-{
-    header("Location: app/dashboard.php");
-} else {
-    header("Location: auth/login.php");
-}
+<?php
