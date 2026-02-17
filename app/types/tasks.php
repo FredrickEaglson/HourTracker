aefbfe47753a5dc08ec97be4af1cf7a2ceec539c
Caleb Baxter <baxtercaleba@gmail.com>
2026-02-16 19:44:04 -0600

    x

--- app/types/tasks.php
+++ app/types/tasks.php
@@ -8,5 +8,12 @@ define("TASK_TYPES",  [
         "icon" => "fa-solid fa-calendar",
         "pathname" => "payperiod",
         "type" => "pp"
+    ],
+    "ulpp" => [
+        "name" => "Use Last Pay Period",
+        "description" => "Use Last Pay Period",
+        "icon" => "fa-solid fa-calendar",
+        "pathname" => "payperiod/uselast",
+        "type" => "ulpp"
     ]
     ]);
